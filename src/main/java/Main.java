7e1d6b643b604119c1441d44c0662fb47e36a53a
sivaprakasam007 <siva4bala9@gmail.com>
2026-04-14 10:46:25 +0530

    Keep container running for Kubernetes deployment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         MessageManager manager = new MessageManager();
-
         manager.create(new Message(1, "Hello world"));
 
         System.out.println("STATUS:FINISHED");
         System.out.println(manager.readAll().get(0).getText());
+
+        // Keep container alive for Kubernetes
+        while (true) {
+            Thread.sleep(10000); // sleep 10 seconds
+        }
     }
 }
