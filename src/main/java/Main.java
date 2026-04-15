75da305f88cc6f9002e542efb18fc4ac68dd94fd
Sabarissh1357 <sabarissh12345@gmail.com>
2026-04-15 18:27:12 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         MessageManager manager = new MessageManager();
-        manager.create(new Message(1, "Hi Sabarissh"));
+        manager.create(new Message(1, "Donation are processed and recorded accurately"));
 
         System.out.println("STATUS:FINISHED");
         System.out.println(manager.readAll().get(0).getText());
